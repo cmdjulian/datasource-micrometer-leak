9c9294fe723e10d3bab04caddfff40e74e01d39a
cmdjulian <julian.goede@pm.me>
2024-05-19 20:20:45 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.5"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("com.h2database:h2")
+	implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:1.0.3")
+	runtimeOnly("com.github.gavlyukovskiy:datasource-proxy-spring-boot-starter:1.9.1")
+	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
